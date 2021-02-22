ac3da89df37fb3d93a28125575d41dabb4d8cb23
Callan Hoskins <callanhoskins@gmail.com>
2021-02-22 14:34:12 -0500

    Add zero-padding to scale_pics

--- scale_pics.py
+++ scale_pics.py
@@ -12,7 +12,9 @@ import cv2
 
 
 files_paths = os.listdir("fish_pics/")
-print(files_paths)
+
+MAX_FAM = '370'
+MIN_FAM = '349'
 
 
 # In[27]:
@@ -24,13 +26,20 @@ def crop_img(img):
     img = img[center[0]-half_len:center[0]+half_len, center[1]-half_len:center[1]+half_len]
     return img
 
+def zero_pad_img(img):
+    height, width, channels = img.shape
+    pad_amt = (width - height) // 2
+    img = cv2.copyMakeBorder(img, pad_amt, pad_amt, 0, 0, cv2.BORDER_CONSTANT, value=0)
+    return img
+
 
 # In[28]:
 
-
-for file in files_paths: 
-    image = cv2.imread('fish_pics/' + file)
-    image = crop_img(image)
-    image = cv2.resize(image, (256, 256))
-    cv2.imwrite('resized_fish_pics/' + file, image)
+if __name__ == '__main__':
+    for file in files_paths:
+        if file <= MAX_FAM and file >= MIN_FAM:
+            image = cv2.imread('fish_pics/' + file)
+            image = zero_pad_img(image)
+            image = cv2.resize(image, (256, 256))
+            cv2.imwrite('resized_fish_pics/' + file, image)
 
